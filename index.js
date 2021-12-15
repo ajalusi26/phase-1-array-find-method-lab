4f23ccb34f0e4d990e823c299dc958669fba93e1
AJ Jalusi <ajalusi26@gmail.com>
2021-12-15 16:47:48 -0500

    commit tree 82fcbaa2eee71cc4e8669789e04e87d8f8c4eb55

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+
+
+function superbowlWin(record){
+    if(record.find(win => win.result === "W") === undefined){
+        return undefined
+    }
+    const found = record.find(win => win.result === "W")
+    return found.year
+}
+
+
+console.log(superbowlWin(record))
